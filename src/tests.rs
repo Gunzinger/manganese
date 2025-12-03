09e7875a4819f4f794fde89a0e928a4d2a32fb96
Daniel Gunzinger <daniel.gunzinger@posteo.de>
2025-12-03 23:25:42 +0100

    fix: comment out segfaulting tests

--- src/tests.rs
+++ src/tests.rs
@@ -17,7 +17,7 @@ pub fn tests_init(cpus: usize, errors: &'static AtomicU64, isa: InstructionSet) 
             unsafe { avx512_tests_init(cpus, errors); }
             vec![
                 Test { name: "Basic Tests", passes: 4, iters: 6, run: avx512_basic_tests },
-                Test { name: "March", passes: 17, iters: 2, run: avx512_march },
+                //FIXME: segfaults Test: Test { name: "March", passes: 17, iters: 2, run: avx512_march },
                 Test { name: "Random Inversions", passes: 4, iters: 16, run: avx512_random_inversions },
                 Test { name: "Moving Inversions <<64", passes: 4, iters: 64, run: avx512_moving_inversions_left_64 },
                 Test { name: "Moving Inversions 32>>", passes: 4, iters: 32, run: avx512_moving_inversions_right_32 },
@@ -26,12 +26,12 @@ pub fn tests_init(cpus: usize, errors: &'static AtomicU64, isa: InstructionSet) 
                 Test { name: "Moving Inversions <<4", passes: 4, iters: 4, run: avx512_moving_inversions_left_4 },
                 Test { name: "Moving Saturations 16>>", passes: 8, iters: 16, run: avx512_moving_saturations_right_16 },
                 Test { name: "Moving Saturations <<8", passes: 8, iters: 8, run: avx512_moving_saturations_left_8 },
-                Test { name: "Addressing", passes: 4, iters: 16, run: avx512_addressing },
+                //FIXME: segfaults Test: Test { name: "Addressing", passes: 4, iters: 16, run: avx512_addressing },
                 Test { name: "SGEMM", passes: 1, iters: 32, run: avx512_sgemm },
                 Test { name: "Walking-1", passes: 4, iters: 64, run: avx512_walking_1 },
                 Test { name: "Walking-0", passes: 4, iters: 64, run: avx512_walking_0 },
                 Test { name: "Checkerboard", passes: 4, iters: 1, run: avx512_checkerboard },
-                Test { name: "Address Line Test", passes: 2, iters: 1, run: avx512_address_line_test },
+                //FIXME: segfaults Test: Test { name: "Address Line Test", passes: 2, iters: 1, run: avx512_address_line_test },
                 Test { name: "Anti-Patterns", passes: 8, iters: 34, run: avx512_anti_patterns },
                 Test { name: "Inverse Data Patterns", passes: 4, iters: 1, run: avx512_inverse_data_patterns },
             ]
@@ -40,7 +40,7 @@ pub fn tests_init(cpus: usize, errors: &'static AtomicU64, isa: InstructionSet) 
             unsafe { avx2_tests_init(cpus, errors); }
             vec![
                 Test { name: "Basic Tests", passes: 4, iters: 6, run: avx2_basic_tests },
-                Test { name: "March", passes: 17, iters: 2, run: avx2_march },
+                //FIXME: segfaultsTest: Test { name: "March", passes: 17, iters: 2, run: avx2_march },
                 Test { name: "Random Inversions", passes: 4, iters: 16, run: avx2_random_inversions },
                 Test { name: "Moving Inversions <<64", passes: 4, iters: 64, run: avx2_moving_inversions_left_64 },
                 Test { name: "Moving Inversions 32>>", passes: 4, iters: 32, run: avx2_moving_inversions_right_32 },
@@ -49,12 +49,12 @@ pub fn tests_init(cpus: usize, errors: &'static AtomicU64, isa: InstructionSet) 
                 Test { name: "Moving Inversions <<4", passes: 4, iters: 4, run: avx2_moving_inversions_left_4 },
                 Test { name: "Moving Saturations 16>>", passes: 8, iters: 16, run: avx2_moving_saturations_right_16 },
                 Test { name: "Moving Saturations <<8", passes: 8, iters: 8, run: avx2_moving_saturations_left_8 },
-                Test { name: "Addressing", passes: 2, iters: 16, run: avx2_addressing },
+                //FIXME: segfaults Test { name: "Addressing", passes: 2, iters: 16, run: avx2_addressing },
                 Test { name: "SGEMM", passes: 1, iters: 32, run: avx2_sgemm },
                 Test { name: "Walking-1", passes: 4, iters: 64, run: avx2_walking_1 },
                 Test { name: "Walking-0", passes: 4, iters: 64, run: avx2_walking_0 },
                 Test { name: "Checkerboard", passes: 4, iters: 1, run: avx2_checkerboard },
-                Test { name: "Address Line Test", passes: 2, iters: 1, run: avx2_address_line_test },
+                //FIXME: segfaults Test { name: "Address Line Test", passes: 2, iters: 1, run: avx2_address_line_test },
                 Test { name: "Anti-Patterns", passes: 8, iters: 34, run: avx2_anti_patterns },
                 Test { name: "Inverse Data Patterns", passes: 4, iters: 1, run: avx2_inverse_data_patterns },
             ]
